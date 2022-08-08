026d0ffcfb0fb02350bf07549c708c9888126848
eunjae lim <ej0514@hanmail.net>
2022-08-08 16:51:23 +0900

    구구단 6단 7단

---
+++ src/Gugudan.java
@@ -0,0 +1,81 @@
+public class Gugudan {
+	public static void main(String[] args) {
+		//2단
+//		System.out.println("2단");
+//		System.out.println(2*1);
+//		System.out.println(2*2);
+//		System.out.println(2*3);
+//		System.out.println(2*4);
+//		System.out.println(2*5);
+//		System.out.println(2*6);
+//		System.out.println(2*7);
+//		System.out.println(2*8);
+//		System.out.println(2*9);
+
+		//3단
+//		System.out.println("3단");
+//		System.out.println(3*1);
+//		System.out.println(3*2);
+//		System.out.println(3*3);
+//		System.out.println(3*4);
+//		System.out.println(3*5);
+//		System.out.println(3*6);
+//		System.out.println(3*7);
+//		System.out.println(3*8);
+//		System.out.println(3*9);
+
+		//4단
+//		int number = 4;
+//		int result = number * 1;
+//		System.out.println(result);
+//		result = number * 2;
+//		System.out.println(result);
+//		result = number * 3;
+//		System.out.println(result);
+//		result = number * 4;
+//		System.out.println(result);
+//		result = number * 5;
+//		System.out.println(result);
+//		result = number * 6;
+//		System.out.println(result);
+//		result = number * 7;
+//		System.out.println(result);
+//		result = number * 8;
+//		System.out.println(result);
+//		result = number * 9;
+//		System.out.println(result);
+
+		// 5단
+//		System.out.println("구구단 중 출력할 단은? : ");
+//		Scanner scanner = new Scanner(System.in);
+//		int number = scanner.nextInt(); // 정수를 입력받아서 변수 num에 저장
+//		System.out.println(number*1);
+//		System.out.println(number*2);
+//		System.out.println(number*3);
+//		System.out.println(number*4);
+//		System.out.println(number*5);
+//		System.out.println(number*6);
+//		System.out.println(number*7);
+//		System.out.println(number*8);
+//		System.out.println(number*9);
+		
+		// 6단
+		int i=1;
+		while(i<10) {
+			System.out.println(6*i);
+			i++;
+		}
+			
+		// 7단
+		for(i=1; i<10; i++) {
+			System.out.println(7*i);
+		}
+		
+		// 8단
+		
+		
+		
+		
+		
+	}
+}
